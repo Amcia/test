71720c32345666e104e17afcf9381304b98c3696
amadeusz <amadeuszmasny@gmail.com>
2018-07-14 17:57:02 +0200

    file save

--- test.py
+++ test.py
@@ -3,9 +3,8 @@ import time
 
 
 print(time.asctime(time.localtime(time.time())))
-for i in range (2):
-    call(["mtr", "-n", "-r", "-c10", "--csv", "-z", "8.8.8.8", ">>", "mtr.log"])
-    print("completed")
-    print(time.asctime(time.localtime(time.time())))
-
-# | awk '{ print $2 $4 }'
+with open("mtr.log", 'w') as f:
+    for i in range (2):
+        call(["mtr", "-n", "-r", "-c10", "--csv", "-z", "8.8.8.8"], stdout=f)
+        print("completed")
+        print(time.asctime(time.localtime(time.time())))
